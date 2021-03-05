27000eb734e2e9c7973ab1202d0be6a47c083856
KRATI SHARMA <KRATI SHARMA@DESKTOP-G2AEBHO>
2021-03-06 00:56:16 +0530

    Commit Message : My demo commit

---
+++ src/main/java/tms_daotest/Test.java
@@ -0,0 +1,16 @@
+package tms_daotest;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("1");
+		
+		System.out.println("2");
+		
+		System.out.println("3");
+		
+		
+
+	}
+
+}
